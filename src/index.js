6da6c2725f68c0c0af7301a3982d788486130fe5
Michael5366 <qwentin536@gmail.com>
2024-08-12 04:23:37 +0300

    feat: finished the morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const result = [];
+    let char = '';
+    let charCode = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        const space = expr.slice(i, i + 10);
+
+        if (space === '**********') {
+
+            result.push(' ');
+
+        } else {
+
+            char = '';
+
+            for (let j = 0; j < 10; j += 2) {
+                charCode = expr.slice(i + j, i + j + 2);
+
+                if (charCode === '10') {
+                    char += '.';
+                } else if (charCode === '11') {
+                    char += '-';
+                }
+            }
+
+            result.push(MORSE_TABLE[char]);
+        }
+    }
+
+    return result.join('');
 }
 
 module.exports = {
